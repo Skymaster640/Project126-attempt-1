8742a69b92f20aeb6ee0f4e54e7e45c598f7ee44
Skymaster640 <skylar.TheHouseRobinson@gmail.com>
2022-02-07 17:04:58 -0600

    Add files via upload

---
+++ classifier.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+import pandas as pd
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import seaborn as sns
+import matplotlib.pyplot as plt
+from PIL import Image
+import PIL.ImageOps
+
+
+X = np.load('image.npz')['arr_0']
+y = pd.read_csv("labels.csv")['labels']
+print(pd.Series(y).value_counts())
+classes = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+nclasses = len(classes)
+
+#samplesperclass = 5
+#figure = plt.figure(figsize=(nclasses*2,(1+samplesperclass*2)))
+
+#idx_cls = 0
+#for cls in classes:
+  #idxs = np.flatnonzero(y == cls)
+  #idxs = np.random.choice(idxs, samplesperclass, replace=False)
+  #i = 0
+  #for idx in idxs:
+    #plt_idx = i * nclasses + idx_cls + 1
+    #p = plt.subplot(samplesperclass, nclasses, plt_idx);
+    #p = sns.heatmap(np.reshape(X[idx], (22,30)), cmap=plt.cm.gray,
+                    #xticklabels=False,yticklabels=False,cbar=False);
+    #p = plt.axis('off');
+    #i += 1
+  #idx_cls +=1
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=9,train_size=7500,test_size=2500)
+X_train_scaled = X_train/255.0
+X_test_scaled = X_test/255.0
+
+clf = LogisticRegression(solver='saga',multi_class='multinomial').fit(X_train_scaled,y_train)
+y_pred = clf.predict(X_test_scaled)
+accuracy = accuracy_score(y_test,y_pred)
+print(accuracy)
+
+#cm = pd.crosstab(y_test,y_pred,rownames=['Actual'], colnames=['Predicted'])
+
+#p = plt.figure(figsize=(10,10));
+#p = sns.heatmap(cm, annot=True, fmt='d', cbar=False)
+
+
+def get_prediction(image):
+    im_pil = Image.open(image)
+    image_bw = im_pil.convert('L')
+    image_bw_resized = image_bw.resize((22,30), Image.ANTIALIAS)
+    pixel_filter = 20
+    min_pixel = np.percentile(image_bw_resized, pixel_filter)
+    image_bw_resized_inverted_scaled = np.clip(image_bw_resized-min_pixel, 0, 255)
+
+    max_pixel = np.max(image_bw_resized)
+
+    image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled)/max_pixel
+
+    test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,660)
+
+    test_pred = clf.predict(test_sample)
+
+    return test_pred[0]
